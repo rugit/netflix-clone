4136885c8b75daf63fa90026dfc383e560c74a46
Anupal Neog <neog.anupal101@gmail.com>
2020-11-07 03:20:52 +0530

    Add posters for movies and make them horizontal

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import './App.css';
+import  Row from  './Row';
+import requests from './requests'
 
 function App() {
   return (
     <div className="App">
       <h1>NETFLIX CLONE</h1>
+      <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals} />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+
+
     </div>
   );
 }
--- src/requests.js
+++ src/requests.js
@@ -2,14 +2,20 @@ const API_KEY = "ddfc40846d283d75e11749173ebbcb92";
 
 const requests = {
 
-    fetchTrending: '/trending/all/week?api_key=${API_KEY}&language=en-US',
-    fetchNetflixOriginals: '/discover/tv?api_key${API_KEY}&with_networks=213',
-    fetchTopRated: '/movie/top_rated?api_key=${API_KEY}&language=en-US',
-    fetchActionMovies: '/discover/movie?api_key=${API_KEY}&with_genres=28',
-    fetchComedyMovies: '/discover/movie?api_key=${API_KEY}&with_genres=35',
-    fetchHorrorMovies: '/discover/movie?api_key=${API_KEY}&with_genres=27',
-    fetchRomanceMovies: '/discover/movie?api_key=${API_KEY}&with_genres=10749',
-    fetchDocumentaries: '/discover/movie?api_key=${API_KEY}&with_genres=99',
+    fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+    fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+    fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+    fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+    fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+    fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+    fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+    fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+    fetchAnimation: `/discover/movie?api_key=${API_KEY}&with_genres=16`,
+    fetchFamilyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10751`,
+    fetchAdventureMovies: `/discover/movie?api_key=${API_KEY}&with_genres=12`,
+    fetchMysteryMovies: `/discover/movie?api_key=${API_KEY}&with_genres=9648`,
+    fetchScienceFictionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=878`,
+
 }
 
 export default requests;

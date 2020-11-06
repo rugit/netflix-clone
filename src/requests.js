8e88e402a3dd76276a475d2f1a5d240ddebcf054
Anupal Neog <neog.anupal101@gmail.com>
2020-11-07 00:27:10 +0530

    first commit

---
+++ src/requests.js
@@ -0,0 +1,9 @@
+const API_KEY = "ddfc40846d283d75e11749173ebbcb92";
+
+const requests = {
+
+    fetchTrending: '/trending/all/week?api_key=${API_KEY}&language=en-US',
+
+}
+
+export default requests;
